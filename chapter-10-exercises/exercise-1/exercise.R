a55a583a340f6d40d883a6b839109ddc0731f90c
Sam Wang <samw1@uw.edu>
2020-04-20 16:00:46 -0700

    ch10-1,2,3

--- chapter-10-exercises/exercise-1/exercise.R
+++ chapter-10-exercises/exercise-1/exercise.R
@@ -2,28 +2,28 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
-
+seahawks_scores <- c(22, 19, 23, 17)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+opp_scores <- c(14, 25, 15, 15)
 
 # Combine your two vectors into a dataframe called `games`
-
+games <- data.frame(seahawks_scores, opp_scores)
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+games$diff <- seahawks_scores - opp_scores
 
 # Create a new column "won" which is TRUE if the Seahawks won the game
-
+games$won <- games$diff > 0
 
 # Create a vector of the opponent names corresponding to the games played
-
+opp_names <- c("Broncos", "Vikings", "Chargers", "Raiders")
 
 # Assign your dataframe rownames of their opponents
-
+row.names(games) <- opp_names
 
 # View your data frame to see how it has changed!
 
--- chapter-10-exercises/exercise-3/exercise.R
+++ chapter-10-exercises/exercise-3/exercise.R
@@ -2,43 +2,48 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(data("USPersonalExpenditure")) 
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+USPE <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+# X1940, X1945, X1950, X1955, X1960
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
 # What are the row names of your dataframe?
-
+# Food and Tobacco, Household Operation, Medical and Health, Personal Care, Private Education
 
 # Add a column "category" to your data frame that contains the rownames
-
+USPE$category <- c("Food and Tobacco", "Household Operation", "Medical and Health",
+                    "Personal Care", "Private Education")
 
 # How much money was spent on personal care in 1940?
-
+USPE["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+USPE["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
-
+USPE[USPE$X1960 == max(USPE$X1960), "category"]
 
 # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
+lowest_category <- function(year) {
+  y <- paste0("X", year)
+  return(USPE[USPE[[y]] == min(USPE[[y]]), "category"])
+}
 
 
 # Using your function, determine the lowest spending category of each year
 # Hint: use the `sapply()` function to apply your function to a vector of years
-
+sapply(seq(1940, 1960, 5), lowest_category)
