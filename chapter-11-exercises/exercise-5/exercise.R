9bc1b938d22bae63a403f20192cccd87c3fe0deb
Sam Wang <samw1@uw.edu>
2020-04-29 16:16:17 -0700

    ch11 4,5,6

--- chapter-11-exercises/exercise-5/exercise.R
+++ chapter-11-exercises/exercise-5/exercise.R
@@ -9,23 +9,37 @@ library("dplyr")
 # What was the average departure delay in each month?
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
-
+month_group <- group_by(flights, month)
+dep_delay_by_month <- month_group %>%
+  summarize(avg_dep_delay = mean(dep_delay, na.rm = TRUE))
 
 # Which month had the greatest average departure delay?
-
+filter(dep_delay_by_month, avg_dep_delay == max(avg_dep_delay, na.rm = TRUE))
+# july
 
 # If your above data frame contains just two columns (e.g., "month", and "delay"
 # in that order), you can create a scatterplot by passing that data frame to the
 # `plot()` function
-
+plot(dep_delay_by_month)
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
+group_dest <- group_by(flights, dest)
+dep_delay_by_dest <- group_dest %>%
+  summarize(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) %>% 
+  arrange(-avg_dep_delay)
 
-
+head(dep_delay_by_dest, 5)
 # You can look up these airports in the `airports` data frame!
-
+# View(airports)
 
 # Which city was flown to with the highest average speed?
+avg_speed <- flights %>% 
+  mutate(avg_speed = distance / air_time)
 
+group_speed_dest <- group_by(avg_speed, dest)
+
+city_speeds <- group_speed_dest %>% 
+  summarize(avg_speed = mean(avg_speed, na.rm = TRUE)) %>% 
+  arrange(-avg_speed)
--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -10,9 +10,19 @@ library("dplyr")
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
+group_dest <- group_by(flights, dest)
+arr_delay_by_dest <- group_dest %>%
+  summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  arrange(-avg_arr_delay)
 
+join_airports <- left_join(arr_delay_by_dest, airports, by = c("dest" = "faa"))
 
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
+group_airline <- group_by(flights, carrier, dest)
+arr_delay_by_carrier <- group_airline %>% 
+  summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  arrange(-avg_arr_delay)
 
+joined <- left_join(arr_delay_by_carrier, airports, by = c("dest" = "faa"))
