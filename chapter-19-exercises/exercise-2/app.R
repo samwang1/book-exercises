713d7ffc43af8f18cfa620165588663f28c6fdbc
Sam Wang <samw1@uw.edu>
2020-05-21 15:04:03 -0700

    complete 19.1/2/3

--- chapter-19-exercises/exercise-2/app.R
+++ chapter-19-exercises/exercise-2/app.R
@@ -1,40 +1,44 @@
 # Exercise 2: a basic Shiny app
 
 # Load the `shiny` package
-
+library(shiny)
 
 # Define a new `ui` variable. This variable should be assigned a `fluidPage()` layout
 # The `fluidPage()` layout should be passed the following:
-
+ui <- fluidPage(
   
   # A `titlePanel()` layout with the text "Cost Calculator"
-
+  titlePanel("Cost Calculator"),
   
   # A `numericInput()` widget with the label "Price (in dollars)"
   # It should have a default value of 0 and a minimum value of 0
   # Hint: look up the function's arguments in the documentation!
-
+  numericInput(label = "Price (in dollars)",
+               min = 0, value = 0, inputId = "price"),
   
   # A second `numericInput()` widget with the label "Quantity"
   # It should have a default value of 1 and a minimum value of 1  
-
+  numericInput(label = "Quantity",
+               min = 1, value = 1, inputId = "qty"),
   
   # The word "Cost", strongly bolded
-
+  strong("Cost"),
   
   # A `textOutput()` output of a calculated value labeled `cost`
-
+  textOutput(outputId = "cost"))
 
 
 # Define a `server` function (with appropriate arguments)
 # This function should perform the following:
-
+server <- function(input, output) {
   
   # Assign a reactive `renderText()` function to the output's `cost` value
   # The reactive expression should return the input `price` times the `quantity`
   # So it looks nice, paste a "$" in front of it!
-
-
+  output$cost <- renderText({
+    return(paste0("$", input$price * input$quantity))
+  })
+}
 
 # Create a new `shinyApp()` using the above ui and server
-
+shinyApp(ui = ui, server = server)
--- chapter-19-exercises/exercise-3/app.R
+++ chapter-19-exercises/exercise-3/app.R
@@ -12,11 +12,11 @@ diamonds_sample <- sample_n(diamonds, 1000)
 
 # For convenience store the `range()` of values for the `price` column
 # (of your sample)
-
+price_range <- range(diamonds_sample$price)
 
 # For convenience, get a vector of column names from the `diamonds` data set to
 # use as select inputs
-
+cols <- colnames(diamonds_sample)
 
 # To help keep the code organized, we'll store some UI elements in variables
 # _before_ defining the UI.
@@ -27,50 +27,63 @@ diamonds_sample <- sample_n(diamonds, 1000)
 # - a label of "Price (in dollars)"
 # - min and max valuesvalue based on the `price_range` calculated above
 # - a current value equal to the price range
-
+price_input <- sliderInput(inputId = "price_choice",
+                           label = "Price (in dollars)",
+                           min = price_range[1],
+                           max = price_range[2],
+                           value = price_range)
 
 # Define a variable `feature_input` that is a `selectInput()` with the
 # label "Feature of Interest". This dropdown should let the user pick one of
 # the columns of the diamond data set. Use the `carat` column as a default
 # Make sure to set an inputId to reference in your server!
-
+feature_input <- selectInput(inputId = "feature",
+                             label = "Feature of Interest",
+                             choices = cols,
+                             selected = "carat")
 
 # Define a UI using a `fluidPage()` layout with the following content:
-
+ui <- fluidPage(
   # A `titlePanel` with the title "Diamond Viewer"
-
+  titlePanel("Diamond Viewer"),
   # Your `prince_input`
-
+  price_input,
   # Your `feature_input`
-
+  feature_input,
   # A `checkboxInput()` labeled "Show Trendline". It's default value is TRUE
-  
+  checkboxInput(inputId = "line", label = "Show Trendline", value = TRUE),
 
   # A plotOutput showing the 'plot' output (based on the user specifications)
-  
+  plotOutput(outputId = "plot"))
 
 
 # Define a `server` function (with appropriate arguments)
 # This function should perform the following:
-
+server <- function(input, output) {
   # Assign a reactive `renderPlot()` function to the outputted 'plot' value
-  
+  output$plot <- renderPlot({
     # This function should first filter down the `diamonds_sample` data
     # using the input price range (remember to get both ends)!
-    
+    df <- diamonds_sample %>%
+      filter(price > input$price_choice[1] && price < input$price_choice[2])
 
     # Use the filtered data set to create a ggplot2 scatter plot with the
     # user-select column on the x-axis, and the price on the y-axis,
     # and encode the "cut" of each diamond using color
     # Save your plot as a variable.
-    
+    plot <- ggplot(df, aes_string(x=input$feature, y="price", color="cut")) +
+      geom_point()
     # Finally, if the "trendline" checkbox is selected, you should add (+)
     # a geom_smooth geometry (with `se=FALSE`) to your plot
     # Hint: use an if statement to see if you need to add more geoms to the plot
-
+    if (input$line) {
+      plot <- plot +
+        geom_smooth(se=FALSE)
+    }
   
     # Be sure and return the completed plot!
-  
+    plot})
+}
 
 # Create a new `shinyApp()` using the above ui and server
-
+shinyApp(ui = ui, server = server)
